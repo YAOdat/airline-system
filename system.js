de28147c7c6d662aad27b1f9d9bce852140f11b3
Yaser Odat <yaserodat@outlook.com>
2022-11-01 16:34:08 +0000

    n

--- system.js
+++ system.js
@@ -3,9 +3,6 @@ const events = require('./events.js');
 require('./manager.js');
 require('./pilot.js');
 
-// the system should console log the details of each event in the following format:
-// event: {event name} time: {time} details: {airline, flightID, pilotName, destination}
-
 events.on('newFlight', (payload) => {
     console.log(`event: ${payload.event} time: ${payload.time} details: ${payload.Airline}, ${payload.flightID}, ${payload.pilotName}, ${payload.Destination}`);
 });
