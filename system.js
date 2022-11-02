96c3a7ca20e447a9001b5602299e4d9cf04bfd0f
Yaser Odat <108061232+YAOdat@users.noreply.github.com>
2022-11-02 17:29:35 +0200

    Update system.js

--- system.js
+++ system.js
@@ -1,9 +1,48 @@
 'use strict';
-const events = require('./events.js');
-require('./manager.js');
-require('./pilot.js');
 
-events.on('newFlight', (payload) => {
-    console.log(`event: ${payload.event} time: ${payload.time} details: ${payload.Airline}, ${payload.flightID}, ${payload.pilotName}, ${payload.Destination}`);
+const io = require('socket.io')(3001);
+const airline = io.of('/airline');
+
+io.on('connection', (socket) => {
+    socket.on('new-flight', (flightDetails) => {
+        io.emit('new-flight', flightDetails);
+        console.log(`Flight {
+            event: 'new-flight',
+            time: ${new Date().toUTCString()},
+            Details: {
+                airline: '${flightDetails.airline}',
+                flightID: '${flightDetails.flightID}',
+                pilot: '${flightDetails.pilot}',
+                destination: '${flightDetails.destination}',
+            }
+        }`);
+    });
+    socket.on('arrived', (flightDetails) => {
+        io.emit('arrived', flightDetails);
+        console.log(`Flight {
+            event: 'arrived',
+            time: ${new Date().toUTCString()},
+            Details: {
+                airline: '${flightDetails.airline}',
+                flightID: '${flightDetails.flightID}',
+                pilot: '${flightDetails.pilot}',
+                destination: '${flightDetails.destination}',
+            }
+        }`);
+    });
 });
 
+airline.on('connection', (socket) => {
+    socket.on('take-off', (flightDetails) => {
+        console.log(`Flight {
+            event: 'took-off',
+            time: ${new Date().toUTCString()},
+            Details: {
+                airline: '${flightDetails.airline}',
+                flightID: '${flightDetails.flightID}',
+                pilot: '${flightDetails.pilot}',
+                destination: '${flightDetails.destination}',
+            }
+        }`);
+    });
+});
